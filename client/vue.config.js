4b2db829f1ab94c5296a296062793908db766093
zz966 <zz966@uowmail.edu.au>
2020-04-09 21:11:08 +1000

    create client
    create client

---
+++ client/vue.config.js
@@ -0,0 +1,14 @@
+// const path = require('path');
+
+// module.exports = {
+//     devServer:{
+//         proxy:'http://localhost:3000'    
+//         // proxy:{
+//         //     '/api':{
+//         //         target: 'http://localhost:3000/',
+//         //         changeOrigin: true,
+//         //     }
+
+//         //}
+//     }
+// };
